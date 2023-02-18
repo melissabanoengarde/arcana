3baaa261af6f05c3a32165ecae8f88fb5900aaad
melissa banoen garde <m.banoengarde@gmail.com>
2023-02-18 16:15:25 -0500

    Randomize each card's position and rotation values

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import * as THREE from "three";
-import { useTexture } from "@react-three/drei";
+import { useTexture, Float } from "@react-three/drei";
 
-const Card = () => {
-  const texture = useTexture("cards/wands/w01.jpg");
+function rd(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const Card = ({ img }) => {
+  const texture = useTexture(`cards/major/${img}`);
 
   return (
-    <mesh>
-      <planeGeometry args={[2, 3.5]} />
-      <meshBasicMaterial map={texture} />
+    <Float
+      speed={1}
+      rotationIntensity={1}
+      floatIntensity={1}
+      floatingRange={[-1, 1]}
+    >
+      <mesh
+        position={[rd(-10, 10), rd(-10, 10), rd(-10, 5)]}
+        rotation={[rd(-0.5, 0.5), rd(-0.5, 0.5), rd(-0.5, 0.5)]}
+      >
+        <planeGeometry args={[2, 3.5]} />
+        <meshStandardMaterial map={texture} />
 
-      {/* back */}
-      <mesh position={[0, 0, -0.001]} scale={1.02}>
-        <planeGeometry args={[2, 3.5, 30, 40]} />
-        <meshBasicMaterial color={"white"} wireframe side={THREE.DoubleSide} />
+        {/* back */}
+        <mesh position={[0, 0, -0.008]} scale={1.02}>
+          <planeGeometry args={[2, 3.5, 30, 40]} />
+          <meshBasicMaterial
+            color={"white"}
+            wireframe
+            side={THREE.DoubleSide}
+          />
+        </mesh>
       </mesh>
-    </mesh>
+    </Float>
   );
 };
 
--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { OrbitControls } from "@react-three/drei";
 import Card from "./Card";
+import Deck from "../deck";
 
 const Stage = () => {
+  const deck = Deck.cards;
+  const major = deck.filter((x) => x.arcana === "Major Arcana");
+  console.log(major.map((x) => x.img));
+
   return (
     <>
       <OrbitControls />
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.8} />
 
       <group>
-        <Card />
+        {major.map((x) => (
+          <Card key={x.name} img={x.img} />
+        ))}
       </group>
     </>
   );
